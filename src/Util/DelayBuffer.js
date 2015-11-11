5730bde95ceb91ce967f01a2e95fde0c50ee25b7
Anton Olofsson <anton.olofsson@gmail.com>
2015-11-11 18:49:01 +0100

    DelayBuffer and mod function added.

---
+++ src/Util/DelayBuffer.js
@@ -0,0 +1,62 @@
+"use strict";
+/*global Float32Array*/
+/*global logError*/
+/*global mod*/
+/*global verify*/
+/*global addTest*/
+
+function DelayBuffer(len) {
+    this.bufferSize = len;
+    this.buffer = new Float32Array(this.bufferSize);
+    this.buffer.fill(0);
+    this.setIndex = 0;
+}
+
+DelayBuffer.prototype.get = function (delay) {
+    return this.buffer[mod(this.setIndex - delay, this.bufferSize)];
+};
+
+DelayBuffer.prototype.set = function (val) {
+    this.setIndex += 1;
+    this.setIndex %= this.bufferSize;
+    this.buffer[this.setIndex] = val;
+};
+
+DelayBuffer.prototype.toString = function () {
+    var i,
+        str = "";
+    
+    for (i = 0; i < this.buffer.length; i += 1) {
+        str += this.buffer[i] + ", ";
+    }
+    return str;
+};
+
+function test_DelayBuffer() {
+    var rb = new DelayBuffer(4);
+    verify(rb.get(0), 0);
+    verify(rb.get(1), 0);
+    verify(rb.get(2), 0);
+    verify(rb.get(3), 0);
+    rb.set(0);
+    rb.set(1);
+    rb.set(2);
+    rb.set(3);
+    verify(rb.get(0), 3);
+    verify(rb.get(1), 2);
+    verify(rb.get(2), 1);
+    verify(rb.get(3), 0);
+    rb.set(4);
+    verify(rb.get(0), 4);
+    verify(rb.get(1), 3);
+    verify(rb.get(2), 2);
+    verify(rb.get(3), 1);
+    rb.set(5);
+    rb.set(6);
+    verify(rb.get(3), 3);
+    verify(rb.get(1), 5);
+    verify(rb.get(2), 4);
+    verify(rb.get(0), 6);
+}
+
+addTest(test_DelayBuffer, "DelayBuffer");
---
+++ src/Util/math.js
@@ -0,0 +1,29 @@
+"use strict";
+/*global verify*/
+/*global addTest*/
+
+function mod(arg, base) {
+    if (base < 0) {
+        base = -base;
+    }
+    if (arg < 0) {
+        return base - ((-arg) % base);
+    }
+    return arg % base;
+}
+
+function test_mod() {
+    verify(mod(-1, 4), 3);
+    verify(mod(-4, 12), 8);
+    verify(mod(4, 12), 4);
+    verify(mod(3, 2), 1);
+    verify(mod(-1, 1), 1);
+    verify(mod(-3, 3), 3);
+    verify(mod(3, 3), 0);
+    verify(mod(3, -3), 0);
+    verify(mod(-1, -3), 2);
+    verify(mod(-4, -3), 2);
+    verify(mod(-4, 3), 2);
+    verify(mod(4, -3), 1);
+}
+addTest(test_mod, "mod()");
